a5c0394c0508735014f3aa5a14c55b76bf6de3b9
MrPró <procontilde@yahoo.com>
2024-06-11 18:11:17 +0200

    Update Empresa.java

--- src/modelo/Empresa.java
+++ src/modelo/Empresa.java
@@ -6,115 +6,160 @@ import java.util.List;
 import java.util.Map;
 
 public class Empresa {
+    
+    private List<Cliente> misClientes;
+    private List<Vehiculo> misVehiculos;
+    private List<Empleado> misEmpleados;
+    private Map<Integer, Reserva> mapaReservas;
 
-	private List<Cliente> misClientes;
-	private List<Vehiculo> misVehiculos;
-	private List<Empleado> misEmpleados;
-	private Map<Integer, Reserva> mapaReservas;
-	
-	public Empresa() {
-	
-		this.misClientes = new ArrayList<>();
-		this.misVehiculos = new ArrayList<>();
-		this.misEmpleados = new ArrayList<>();
-		this.mapaReservas = new HashMap<>();
-	
-	}
-	
-	public boolean agregarCliente(Cliente cliente) {
-	
-		if(cliente != null) {
-	
-			this.misClientes.add(cliente);
-			return true;
-	
-		}
-			
-		return false;
-	
-	}
-	
-	public boolean agregarVehiculo(Vehiculo vehiculo) {
-	
-		
-		if(vehiculo != null && !misVehiculos.contains(vehiculo)) {
-			
-			this.misVehiculos.add(vehiculo);	
-			return true;
-	
-		}
-		
-		return false;
-	
-	}
-	
-	public void agregarEmpleado(Empleado empleado) {
-	
-		if(empleado != null && !misEmpleados.contains(empleado)) this.misEmpleados.add(empleado);
-	
-	}
-	
-	public Cliente buscarClientePorDni(String dni) {
-	
-		for (Cliente cliente : misClientes) if (cliente.getDni().equals(dni)) return cliente;
-		return null;
-	
-	}
-	
-	public Vehiculo buscarVehiculoPorMatricula(String matricula) {
-	
-		for (Vehiculo vehiculo : misVehiculos) if (vehiculo.getMatricula().equals(matricula)) return vehiculo;
-		return null;
-		
-	}
-	
-	public boolean agregarReserva(int numeroReserva, Reserva reserva) {
-	
-		if (reserva != null && !mapaReservas.containsKey(numeroReserva)) {
-			
-		    mapaReservas.put(numeroReserva, reserva);
-		    
-		    return true;
-		    
-		}
-		
-		return false;
-	
-	}
-	
-	
-	public Reserva encontrarReserva(int numeroReserva) {
-	
-		return mapaReservas.get(numeroReserva);
-	
-	}
-	
-	public boolean actualizarReserva(int numeroReserva, Reserva nuevaReserva) {
-	
-		if (mapaReservas.containsKey(numeroReserva) && nuevaReserva != null) {
-			
-		    mapaReservas.put(numeroReserva, nuevaReserva);
-		    return true;
-		    
-		}
-		
-		return false;
-		
-	}
-	
-	public boolean eliminarReserva(int numeroReserva) {
-	
-		if (mapaReservas.containsKey(numeroReserva)) {
-			
-		    mapaReservas.remove(numeroReserva);
-		    return true;
-		    
-		}
-		
-		return false;
-	
-	}
+    public Empresa() {
+        
+        this.misClientes = new ArrayList<>();
+        this.misVehiculos = new ArrayList<>();
+        this.misEmpleados = new ArrayList<>();
+        this.mapaReservas = new HashMap<>();
+        
+    }
 
+    public boolean agregarCliente(Cliente cliente) {
+        
+        if(cliente != null) {
+        	
+            this.misClientes.add(cliente);
+            return true;
+            
+        }
+        
+        return false;
+        
+    }
+    
+    public boolean eliminarCliente(String dni) {
+        
+        Cliente cliente = buscarClientePorDni(dni);
+        
+        if(cliente != null) {
+        	
+            this.misClientes.remove(cliente);
+            return true;
+            
+        }
+        
+        return false;
+        
+    }
+
+    public boolean agregarVehiculo(Vehiculo vehiculo) {
+        
+        if(vehiculo != null && !misVehiculos.contains(vehiculo)) {
+        	
+            this.misVehiculos.add(vehiculo);
+            return true;
+            
+        }
+        
+        return false;
+        
+    }
+
+    public boolean eliminarVehiculo(String matricula) {
+        
+        Vehiculo vehiculo = buscarVehiculoPorMatricula(matricula);
+        
+        if(vehiculo != null) {
+        	
+            this.misVehiculos.remove(vehiculo);
+            return true;
+            
+        }
+        
+        return false;
+        
+    }
+
+    public void agregarEmpleado(Empleado empleado) {
+        
+        if(empleado != null && !misEmpleados.contains(empleado)) this.misEmpleados.add(empleado);
+        
+    }
+
+    public boolean eliminarEmpleado(String dni) {
+        
+        Empleado empleado = encontrarEmpleado(dni);
+        
+        if(empleado != null) {
+        	
+            this.misEmpleados.remove(empleado);
+            return true;
+            
+        }
+        
+        return false;
+        
+    }
+
+    public Empleado encontrarEmpleado(String dni) {
+        
+        for (Empleado empleado : misEmpleados) if (empleado.getDni().equals(dni)) return empleado;
+        return null;
+        
+    }
+
+    public Cliente buscarClientePorDni(String dni) {
+        
+        for (Cliente cliente : misClientes) if (cliente.getDni().equals(dni)) return cliente;
+        return null;
+    }
+
+    public Vehiculo buscarVehiculoPorMatricula(String matricula) {
+        
+        for (Vehiculo vehiculo : misVehiculos) if (vehiculo.getMatricula().equals(matricula)) return vehiculo;
+        return null;
+        
+    }
+    
+    public boolean agregarReserva(int numeroReserva, Reserva reserva) {
+        
+        if (reserva != null && !mapaReservas.containsKey(numeroReserva)) {
+        	
+            mapaReservas.put(numeroReserva, reserva);
+            return true;
+            
+        }
+        
+        return false;
+        
+    }
+
+    public Reserva encontrarReserva(int numeroReserva) {
+        
+        return mapaReservas.get(numeroReserva);
+    }
+
+    public boolean actualizarReserva(int numeroReserva, Reserva nuevaReserva) {
+        
+        if (mapaReservas.containsKey(numeroReserva) && nuevaReserva != null) {
+        	
+            mapaReservas.put(numeroReserva, nuevaReserva);
+            return true;
+            
+        }
+        
+        return false;
+        
+    }
+
+    public boolean eliminarReserva(int numeroReserva) {
+        
+        if (mapaReservas.containsKey(numeroReserva)) {
+        	
+            mapaReservas.remove(numeroReserva);
+            return true;
+            
+        }
+        
+        return false;
+        
+    }
+    
 }
-
-
